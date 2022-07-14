71e64ae557b7742505ff570bb068f737c7040458
Sivagajan <sivagajan@gmail.com>
2022-07-14 13:10:22 +0200

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,9 @@
+
+
+let helloworld = 'Hello World'
+const pi = 3.14 
+const burjDubai = '828m'
+let fullName = 'Jan Schmidt'
+const eifelTower = '324m'
+let camelCase = 'camelCase'
+kursStatus = Boolean (true)
